d7b42aa56d25dbba94d919bd102355ae1440c9f1
PrataCat <111517381+PrataCat@users.noreply.github.com>
2023-02-04 14:08:20 +0000

    Deploying to gh-pages from @ PrataCat/goit-js-hw-09@f9a0d59c0c3eba6a6f35b00f6c2073351271a7e8 🚀

---
+++ 01-color-switcher.503664a0.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n=null;e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.503664a0.js.map
