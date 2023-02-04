f9a0d59c0c3eba6a6f35b00f6c2073351271a7e8
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-02-04 16:07:50 +0200

    task1 completely & task2 css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timer = null;
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timer);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
