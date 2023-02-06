4dbb67fc9e5a454b712dcc44fec5fb26b550f0f3
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-02-06 23:28:29 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,67 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  timeout: 4000,
+  clickToClose: true,
+});
+
+const formField = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
+let promiseСounter = 1;
+let delayTime = 0;
+let stepTime = 0;
+let amountNumber = 0;
+
+formField.addEventListener('change', () => {
+  delayTime = +delay.value;
+  stepTime = +step.value;
+  amountNumber = +amount.value;
+});
+
+formField.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  actingPromise();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
+
+function actingPromise() {
+  const PromiseTimerId = setInterval(() => {
+    if (promiseСounter <= amountNumber) {
+      createPromise(promiseСounter, delayTime)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        })
+        .finally(() => {
+          promiseСounter += 1;
+          delayTime += stepTime;
+        });
+    } else {
+      clearInterval(PromiseTimerId);
+      promiseСounter = 1;
+      delayTime = 0;
+      stepTime = 0;
+      amountNumber = 0;
+    }
+  }, delayTime);
 }
